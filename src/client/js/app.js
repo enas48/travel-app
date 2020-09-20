68bd9d7b52204c8e171ca0ec65ad37b92c8255fa
enas48 <enasm4829@gmail.com>
2020-09-20 16:06:57 +0200

    update

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -313,7 +313,7 @@ const getWeather = async data => {
 const postData = async (url = '', data = {}) => {
   const response = await fetch(url, {
     method: 'POST',
-    credentials: 'same-origin',
+    credentials: 'include',
     headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(data)
   });
